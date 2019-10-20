c3589a2fc7e5c5e97e0619d4d08b5d387a77ec74
radha-soni <er.radha17@gmail.com>
2019-10-20 13:50:54 +0530

    Updates

---
+++ precache-manifest.4ebf3b1b179c5f6e22afc000eff1950b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3bfb3560371bdb40d264ba9d86530fe8",
+    "url": "/js-calculator/index.html"
+  },
+  {
+    "revision": "390e557077a3a1f66371",
+    "url": "/js-calculator/static/css/main.eb83d620.chunk.css"
+  },
+  {
+    "revision": "5bb46f81230ed7854494",
+    "url": "/js-calculator/static/js/2.068d7593.chunk.js"
+  },
+  {
+    "revision": "390e557077a3a1f66371",
+    "url": "/js-calculator/static/js/main.44a9c7b8.chunk.js"
+  },
+  {
+    "revision": "846fd0d54d1b43db2b74",
+    "url": "/js-calculator/static/js/runtime-main.91adad31.js"
+  }
+]);
